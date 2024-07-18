1db48d46d434426d722b6429c431a7db9505cbd3
Fauzan FL <fauzanarth@gmail.com>
2024-07-18 10:58:10 +0700

    chore: add cors and session config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+require('dotenv').config({ path: __dirname + '/../.env' });
+const { SESSION_SECRET, CLIENT_URL } = process.env;
 
 const app = express();
 const port = 5000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Welcome to transaki api!');
 });
 
 app.listen(port, () => {
