b48c26efa2e1bc939cff16af018bad4603a2ead5
Fauzan FL <fauzanarth@gmail.com>
2024-07-19 08:24:05 +0700

    fix: change jwt expires time

--- src/utils/token.js
+++ src/utils/token.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: __dirname + '/../../.env' });
 
 function generateAccessToken(username) {
   return jwt.sign({ username: username }, process.env.JWT_SECRET, {
-    expiresIn: '1h',
+    expiresIn: '3h',
   });
 }
 
