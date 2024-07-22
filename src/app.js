6dfa8d881dbbfd2777cff02fe005269cdb79279c
Fauzan FL <fauzanarth@gmail.com>
2024-07-22 11:28:21 +0700

    add cors method

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   })
 );
 
